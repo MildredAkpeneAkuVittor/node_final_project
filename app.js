7a65b760da2af5a27dd03b5e49fe0ad2c7754e3c
mildred.vittor@amalitech.org <mildred.vittor@amalitech.org>
2021-08-19 17:26:37 +0000

    new

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const adminroutes = require('./routes/adminroutes');
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const initialized = require('./config/passportconfig');
   initialized(passport);
